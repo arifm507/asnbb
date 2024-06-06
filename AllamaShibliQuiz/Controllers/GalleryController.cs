f262cf0e891677a0b5df1a1838df29978b3359a8
arifm507 <arifm507@gmail.com>
2024-06-07 02:32:45 +0530

    fixed

---
+++ AllamaShibliQuiz/Controllers/GalleryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllamaShibliQuiz.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: GalleryController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
