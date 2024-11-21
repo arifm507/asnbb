43754d25b29171fdb4a9000180f326d3189d38aa
arifm507 <arifm507@gmail.com>
2024-11-22 00:32:48 +0530

    answer key

--- AllamaShibliQuiz/Controllers/AnswerKeyController.cs
+++ AllamaShibliQuiz/Controllers/AnswerKeyController.cs
@@ -1,32 +1,31 @@
-﻿using AllamaShibliQuiz.Models.ViewModels;
-using Microsoft.AspNetCore.Http;
+﻿using AllamaShibliQuiz.Models.RequestModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AllamaShibliQuiz.Controllers
 {
-    public class ResultController : Controller
+    public class AnswerKeyController : Controller
     {
         private IWebHostEnvironment Environment;
 
-        public ResultController(IWebHostEnvironment _environment)
+        public AnswerKeyController(IWebHostEnvironment _environment)
         {
             Environment = _environment;
         }
 
-        // GET: ResultController
         public ActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(ResultViewModel result)
+        public ActionResult Index(AnswerKeyRequestModel result)
         {
             if (ModelState.IsValid)
             {
-                if (result.Class > 0)
+                if (result.Class != "0")
                 {
-                    string path = Path.Combine(this.Environment.WebRootPath, $"results/class_{result.Class}.pdf");
+                    string path = Path.Combine(this.Environment.WebRootPath, $"answer_key/answerkey_{result.BookletCode}_{result.Class}.pdf");
                     if (System.IO.File.Exists(path))
                     {
                         return File(System.IO.File.OpenRead(path), "application/octet-stream", Path.GetFileName(path));
---
+++ AllamaShibliQuiz/Models/RequestModels/AnswerKeyRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AllamaShibliQuiz.Models.RequestModels
+{
+    public class AnswerKeyRequestModel
+    {
+        public int BookletCode { get; set; }
+        public string Class { get; set; }
+    }
+}
